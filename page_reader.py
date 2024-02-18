81c23bc1ff2213b44218252412caf1fab85e7cdc
David Valenta <davidvalenta@Air-von-David.docsis.vodafone.cz>
2024-02-18 23:21:10 +0100

    upload final solution of the assignment

--- page_reader.py
+++ page_reader.py
@@ -1,8 +1,16 @@
-
-from typing import Union, List, Dict
-from enum import Enum
 import argparse
+import os
+from enum import Enum
+from typing import Dict, List, Union
+
 import numpy as np
+import tensorflow as tf
+from scipy.ndimage import zoom
+from skimage import exposure, filters, measure, transform
+from skimage.measure import regionprops
+from sklearn.preprocessing import LabelEncoder
+from sklearn.utils import shuffle
+
 import utils
 
 # NOTE: Arguments in this source file are only for your convenience. They will not be used during
@@ -22,6 +30,8 @@ class PageReader:
         # Argument 'note' is passed to this method by the evaluator. It is a single string argument that you
         # can use for something, e.g., name of the trained/loaded model in the fit method.
         self._phrases = utils.Phrases()
+        self.model_filepath_letters = "saved_model/my_model_letters"
+        self.model_filepath_digits = "saved_model/my_model_digits"
 
     def fit(self, training : bool) -> None:
         # TODO: Place your training, model saving and model loading code in here.
@@ -37,7 +47,91 @@ class PageReader:
         # This method should be able to train your solution on demand. That means, if the argument 'training'
         # is 'True' then you should train your classification models and use the newly trained ones. If the argument
         # is 'False' then you should load models from saved files.
-        raise NotImplementedError()
+
+        # Train new models and save them to files.
+        if training:
+            # Train new models.
+            # In the assignment we assume that line with page number is always separated and therefore
+            # we can train two separated models.
+            self.trainCNN(utils.CharType.LETTER)
+            self.trainCNN(utils.CharType.DIGIT)
+
+        # Load saved models from files.
+        else:
+            if not os.path.exists(self.model_filepath_letters):
+                raise FileNotFoundError()
+            else:
+                self.model_letters = tf.keras.models.load_model(self.model_filepath_letters)
+            if not os.path.exists(self.model_filepath_digits):
+                raise FileNotFoundError()
+            else:
+                self.model_digits = tf.keras.models.load_model(self.model_filepath_digits)
+
+    def trainCNN(self, type: utils.CharType):
+            #=== Choose letter or numeric dataset
+            if type == utils.CharType.LETTER:
+                datasetPath = "emnist_data/emnist_train_big.npz"
+            else:
+                datasetPath = "emnist_data/emnist_train_num.npz"
+
+            my_data = utils.EMnistDataset(datasetPath)
+
+            # Preprocess the data
+            images = np.expand_dims(my_data.imgs, axis=-1)  # Add a channel dimension
+
+            # Encode labels
+            label_encoder = LabelEncoder()
+            labels_encoded = label_encoder.fit_transform(my_data.labels)
+
+            # Just shuffle it for randomness
+            images, labels_encoded = shuffle(images, labels_encoded, random_state=42)
+
+            # Build the CNN model which will be main core of this program
+            model = tf.keras.Sequential([
+                tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+                tf.keras.layers.BatchNormalization(),
+                tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+                tf.keras.layers.BatchNormalization(),
+                tf.keras.layers.MaxPooling2D((2, 2)),
+                tf.keras.layers.Dropout(0.25),
+                
+                tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+                tf.keras.layers.BatchNormalization(),
+                tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+                tf.keras.layers.BatchNormalization(),
+                tf.keras.layers.MaxPooling2D((2, 2)),
+                tf.keras.layers.Dropout(0.25),
+                
+                tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+                tf.keras.layers.BatchNormalization(),
+                tf.keras.layers.MaxPooling2D((2, 2)),
+                tf.keras.layers.Dropout(0.25),
+                
+                tf.keras.layers.Flatten(),
+                tf.keras.layers.Dense(512, activation='relu'),
+                tf.keras.layers.BatchNormalization(),
+                tf.keras.layers.Dropout(0.5),
+                
+                tf.keras.layers.Dense(256, activation='relu'),
+                tf.keras.layers.BatchNormalization(),
+                tf.keras.layers.Dropout(0.5),
+                
+                tf.keras.layers.Dense(len(label_encoder.classes_), activation='softmax')
+            ])
+
+            model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+            # Finally, we train it.
+            model.fit(images, labels_encoded, epochs=15)
+
+            # Save the model to a file and also to this instance of PageReader.
+            if type == utils.CharType.LETTER:
+                model.save(self.model_filepath_letters)
+                self.model_letters = model
+            else:
+                model.save(self.model_filepath_digits)
+                self.model_digits = model
+
 
     def solve(self, pages : np.ndarray) -> Dict[str, Union[List[List[str]], List[str]]]:
         # TODO: This method should solve a single page reading task.
@@ -46,9 +140,93 @@ class PageReader:
         # (both in the input order of the pages) and the final list of movement commands in the correct order
         # according to the page numbers.
 
-        classified_text : List[List[str]] = None
-        matched_phrases : List[List[str]] = None
-        ordered_commands : List[str] = None
+        classified_text : List[List[str]] = []
+        matched_phrases : List[List[str]] = []
+        ordered_commands : List[str] = []
+
+        command_pages_dict = {} #Dict where keys are page numbers and values page lines.
+
+        for i in range(pages.shape[0]): # iterate through pages (which is first dimension in this case)
+            # Access the 2D array at index i
+            current_2D_array = pages[i, :, :]
+            
+            img_array_normalized = exposure.rescale_intensity(current_2D_array)
+            # Apply thresholding
+            threshold_value = filters.threshold_otsu(img_array_normalized)
+            binary_image = img_array_normalized > (threshold_value * 0.05)
+
+            # Label connected components
+            # (Below I will use Levenshtein metric to recognize phrases,
+            # so handling spaces between words is not necessary here.)            
+            label_image = measure.label(binary_image)
+
+            # Get properties of labeled regions
+            regions = regionprops(label_image)
+
+            # Extract bounding boxes of regions
+            bounding_boxes = [region.bbox for region in regions]
+
+            linesWithBoxes = utils.GetLinesByBoundaryBoxes(bounding_boxes, pages.shape[1])
+
+            classified_text.append([])
+            matched_phrases.append([])     
+            pages_command = [] # array of commands on single page
+
+            for radek in range (0, len(linesWithBoxes[0])): # iterate through lines of page
+                # Get bounding boxes within given line
+                subarray_of_line = utils.GetBoxesByLine(bounding_boxes, linesWithBoxes[1][radek], linesWithBoxes[2][radek])
+
+                #reading last line == line with page number
+                lastLine = radek >= len(linesWithBoxes[0]) - 1
+
+                if lastLine:
+                    # In the assignment we assume that the page number consists of four digits, so for further
+                    # recognization I will remove the smallest rectangles that are most likely some noise.
+                    while len(subarray_of_line) > 4:
+                        smallest_rectangle = min(subarray_of_line, key=utils.RectangleArea)
+                        subarray_of_line.remove(smallest_rectangle)
+
+                images_of_chars = [] # Images of single chars from the given line
+                target_size = (28, 28)
+
+                for line_character in subarray_of_line: # iterate through characters of line
+                    # The performance is better if I add some padding to the separated characters.
+                    # The horizontal padding is greater because widths has greater variances.
+                    # (For example letter M is much wider than I, but it has same height).
+                    padding_horizontal = 5
+                    padding_vertical = 2
+                    min_row, min_col, max_row, max_col = line_character[0] - padding_vertical, line_character[1] - padding_horizontal, line_character[2] + padding_vertical, line_character[3] + padding_horizontal
+                    
+                    # Get image of the character from the original input image by boundary box
+                    subimage = img_array_normalized[min_row:max_row + 1, min_col:max_col + 1]
+
+                    # Zoom it to the size 28x28 beacuse model expects this size
+                    scale_factors = (
+                        target_size[0] / subimage.shape[0],
+                        target_size[1] / subimage.shape[1]
+                    )
+                    images_of_chars.append(zoom(subimage, scale_factors, order=3))
+
+                images_of_chars_as_ND = np.stack(images_of_chars) # convert list to np.ndarray to pass it to the model
+
+                if not lastLine: # === Line with command
+                    predictions = self.model_letters.predict(images_of_chars_as_ND)
+                    recognized_text = utils.ProbabilitiesToText(utils.CharType.LETTER, predictions)
+                    command = utils.MostSimilarPhraseLevenshtein(recognized_text, self._phrases.phrasesToList())
+
+                    classified_text[i].append(command)
+                    matched_phrases[i].append(command)
+                    pages_command.append(self._phrases.toCommand(command))
+
+                else: # Last line with page number === last iteration
+                    predictions = self.model_digits.predict(images_of_chars_as_ND)
+                    recognized_text = utils.ProbabilitiesToText(utils.CharType.DIGIT, predictions)
+
+                    classified_text[i].append(recognized_text)
+                    command_pages_dict[int(recognized_text)] = pages_command
+
+        # Order the command sequences by page number and flatten it into a single list
+        ordered_commands = [word for page_number in sorted(command_pages_dict.keys()) for word in command_pages_dict[page_number]]
 
         return {
             # The direct result of character classification including the page numbers. One string for each line.
@@ -68,12 +246,12 @@ def main(args : argparse.Namespace) -> None:
     # your solution can be evaluated with on-demand training.
     #
     # Evaluation of your solution through the commandline can look like this:
-    # >>> python .\evaluator.py --type=single --set=python_train --name=005 --verbose=2 --note=something
+    # >>> python .\evaluator.py --type=single --set=python_train --name=005 --verbose=2 --note=something # toto mi fungovalo at na 007
     # >>> python .\evaluator.py --type=full --set=python_train --verbose=1 --training
     # >>> python .\evaluator.py --type=full --set=python_validation --verbose=1
 
     # You can load maze run files using the following code:
-    mr_path = "page_data/python_train/001.npz"
+    mr_path = "page_data/python_validation/001.npz"
     maze_run = utils.MazeRunLoader.fromFile(mr_path)
     raise NotImplementedError()
 
--- utils.py
+++ utils.py
@@ -1,8 +1,13 @@
-
 from __future__ import annotations
+
 import os
+import sys
+from enum import Enum
 from typing import Sequence, List
+
 import numpy as np
+
+
 
 class EMnistDataset:
     """
@@ -61,6 +66,16 @@ class Phrases:
     
     def toPhrase(self, command : str) -> str:
         return self.command_to_phrase[command]
+    
+    def phrasesToList(self):
+        """
+        Returns all possible phrases as a simple list.
+
+        Returns:
+        List of possible phrases.
+        """
+
+        return self.phrases
 
 class MazeRunLoader:
 
@@ -86,3 +101,233 @@ class MazeRunLoader:
             text.append(list(page_text))
             text[-1].append(number)
         return text
+
+
+class CharType(Enum):
+    LETTER = 0
+    DIGIT = 1
+
+class CharacterDecoder:
+    "Class to handle working with the emnist mapping file."
+
+    def __init__(self, path: str = "emnist_mapping.txt"):
+        self.hodnoty = {}
+        with open(path, 'r') as file:
+            for line in file:
+                key, value = map(int, line.split())
+                self.hodnoty[key] = value
+        self.digit_offset = 10 #first 10 lines of the files are digits
+        print(self.hodnoty)
+
+    def getChar(self, type: CharType, index: int):
+        """
+        Finds correct string of recognized phrase using probabilities
+        of each character.
+
+        Parameters:
+        - type: Determines whether we need to recognice alpha or numeric char.
+                    (In the assigment we assume always separated line with page
+                    number, thus we won't be ever recognizing alphanumeric string.)
+        - index: Index of the character.
+
+        Returns:
+        Decoded character as string.
+        """
+
+        if type == CharType.DIGIT and index >= 0 and index <= 9:
+            return str(chr(self.hodnoty[index]))
+        elif type == CharType.LETTER and index >= 0 and index <= 51:
+            return str(chr(self.hodnoty[index + self.digit_offset]))
+        return ""
+    
+    
+def ProbabilitiesToText(type: CharType, probs: np.ndarray):
+    """
+    Finds correct string of recognized phrase using probabilities
+    of each character.
+
+    Parameters:
+    - type: Determines whether we need to recognice alpha or numeric text.
+                (In the assigment we assume always separated line with page
+                number, thus we won't be ever recognizing alphanumeric string.)
+    - probs: Array with probabilites for each character at every position
+                of recognized phrase.
+
+    Returns:
+    Recognized string.
+    """
+
+    result = ""
+    cd = CharacterDecoder()
+    for prob in probs:
+        max_index = np.argmax(prob)
+        result += cd.getChar(type, max_index)
+    return result
+
+
+def BoxIntersect(arrayOfBoxes, line):
+    """
+    Determines, whether line (given by Y coordinate) intersects
+    any of the provided bounding boxes.
+
+    Parameters:
+    - arrayOfBoxes: Array of bounding boxes.
+    - line: Line Y coordinate.
+
+    Returns:
+    Boolean: Line intersects any box.
+    """
+
+    for box in arrayOfBoxes:
+        if box[0] <= line and box[2] >= line:
+            return True
+    return False
+
+def GetLinesByBoundaryBoxes(arrayOfBoxes, imageHeight):
+    """
+    Finds coordinates of all lines from given set of boundary boxes.
+
+    By line I mean space between two horizontal borders occupied
+    by any of the given boundary boxes.
+
+    We assume, that the lines don't intersect and there is also
+    horizontal border of size at least 1 between the lines.
+
+    Parameters:
+    - arrayOfBoxes: Array of bounding boxes.
+    - line: Pixel height of input image.
+
+    Returns:
+    List of three lists:
+        - List with line indices (I provided here for testing purposes; not necessary otherwise).
+        - List with Y coordinates of lower borders of the lines
+        - List with Y coordinates of upper borders of the lines
+    """
+    readingLine = False
+    readingLineChange = False
+
+    tmpNrOfLines = 0
+    line_indices = []
+    minimum_lines_y = []
+    maximum_lines_y = []
+
+    for i in range(0, imageHeight):
+        if BoxIntersect(arrayOfBoxes, i):
+            if not readingLine:
+                readingLineChange = True
+            else:
+                readingLineChange = False
+            readingLine = True
+            if readingLineChange:
+                line_indices.append(tmpNrOfLines)
+                minimum_lines_y.append(i)
+                tmpNrOfLines += 1
+        else:
+            if readingLine:
+                readingLineChange = True
+            else:
+                readingLineChange = False
+            readingLine = False
+            if (readingLineChange):
+                maximum_lines_y.append(i)
+            
+    return [line_indices, minimum_lines_y, maximum_lines_y]
+
+
+def RectangleArea(rect):
+    """
+    Calculates area of rectangle.
+
+    Parameters:
+    - rect (quadriple): Rectangle given by min_X, min_Y, max_X, max_Y.
+
+    Returns:
+    Rectangle area.
+    """
+
+    min_x, min_y, max_x, max_y = rect
+    return (max_x - min_x) * (max_y - min_y)
+
+def GetBoxesByLine(arrayOfBoxes, min_y, max_y):
+    """
+    Finds all bounding boxes within given boundaries.
+
+    Parameters:
+    - arrayOfBoxes: Array of bounding boxes.
+    - min_Y: Y coordinate of lower boundary.
+    - max_Y: Y coordinate of upper boundary.
+
+    Returns:
+    Bounding boxes within given horizontal lines.
+    """
+
+    boxes_result = []
+    for box in arrayOfBoxes:
+        if box[0] >= min_y and box[2] <= max_y:
+            boxes_result.append(box)
+    return sorted(boxes_result, key=lambda x: x[1])
+
+
+def LevenshteinDistance(str1: str, str2: str):
+    """
+    Calculates Levenshtein (minimum edit) distance of two strings.
+
+    Parameters:
+    - str1 (str): String 1 to compare.
+    - str2 (str): String 2 to compare.
+
+    Returns:
+    Levenshtein distance as integer.
+    """
+
+    len_str1 = len(str1) + 1
+    len_str2 = len(str2) + 1
+
+    # Matrix for distances
+    matrix = [[0 for n in range(len_str2)] for m in range(len_str1)]
+
+    # Init matrix with values corresponding to the lengths of substrings
+    for i in range(len_str1):
+        matrix[i][0] = i
+    for j in range(len_str2):
+        matrix[0][j] = j
+
+    # Populate the matrix using dynamic programming
+    for i in range(1, len_str1):
+        for j in range(1, len_str2):
+            cost = 0 if str1[i - 1] == str2[j - 1] else 1
+            matrix[i][j] = min(
+                matrix[i - 1][j] + 1,  # Deletion
+                matrix[i][j - 1] + 1,  # Insertion
+                matrix[i - 1][j - 1] + cost  # Substitution
+            )
+
+    # Return result Levenshtein
+    return matrix[len_str1 - 1][len_str2 - 1]
+
+
+def MostSimilarPhraseLevenshtein(input_phrase, phrase_list):
+    """
+    For given phrase finds most similar phrase from provided list 
+    using Levenshtein metrics.
+
+    Parameters:
+    - input_phrase (str): String with phrase to classify.
+    - phrase_list (List): List of possible phrases.
+
+    Returns:
+    Most similar phrase.
+    """
+
+    min_distance = sys.maxsize
+    most_similar = None
+
+    for phrase in phrase_list:
+        distance = LevenshteinDistance(input_phrase, phrase)
+        if distance < min_distance:
+            min_distance = distance
+            most_similar = phrase
+
+    return most_similar
+
+
